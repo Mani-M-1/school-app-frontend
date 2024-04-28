95b5474c3facde4dfec34d795d95f362e8511478
Manikanta Muppidi <128898564+Mani-M-1@users.noreply.github.com>
2024-04-28 21:01:12 +0530

    forgo password bug rectified

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,3 +1,46 @@
+// // old logic
+
+// import { Component } from '@angular/core';
+// import { NavigationEnd, Router } from '@angular/router';
+// import { RoleService } from '../role.service';
+
+// @Component({
+//   selector: 'app-tabs',
+//   templateUrl: 'tabs.page.html',
+//   styleUrls: ['tabs.page.scss'],
+// })
+// export class TabsPage {
+//   isStudent = false;
+//   isPrincipal = false;
+//   isProfessor = false;
+
+//   constructor(private roleService: RoleService, private router: Router) {}
+
+//   ngOnInit() {
+//     this.router.events.subscribe((event) => {
+//       if (event instanceof NavigationEnd) {
+//         const userRole = this.roleService.getUserRole();
+//         console.log(`tabs page - role: ${userRole}`);
+//         if (userRole) {
+//           this.updateTabsVisibility(userRole);
+//         }
+//       }
+//     });
+//   }
+
+//   private updateTabsVisibility(userRole: string) {
+//     this.isStudent = userRole === 'student';
+//     this.isProfessor = userRole === 'professor';
+//     this.isPrincipal = userRole === 'principal';
+
+//     console.log(`tabs page - isStudent:${this.isStudent}`);
+//     console.log(`tabs page - isProfessor:${this.isProfessor}`);
+//     console.log(`tabs page - isPrincipal:${this.isPrincipal}`);
+//   }
+// }
+
+// latest logic
+
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { RoleService } from '../role.service';
@@ -28,11 +71,15 @@ export class TabsPage implements OnInit {
   }
 
   private checkUserRole() {
+    const url = this.router.url;
+    console.log('Current URL Path:', url);
+
     const userRole = this.roleService.getUserRole();
     if (userRole) {
       this.updateTabsVisibility(userRole);
       this.isRoleChecked = true;
-    } else {
+    } else if (url !== '/forgot-password') {
+      // avoiding navigation to "sign-in" page when we click on forgot-password button in sign-in page
       this.router.navigate(['/sign-in']);
     }
   }
