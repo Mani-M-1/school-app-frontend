f1532b2a043cd1b2f54e296b17a1decfe75f95c6
Manikanta Muppidi <128898564+Mani-M-1@users.noreply.github.com>
2024-05-21 22:19:36 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
   // apiUrl: 'http://localhost:3000',
 
   // production url
-  apiUrl: 'https://student-corner-production-3fc875bb5b7f.herokuapp.com',
+  apiUrl: 'https://student-corner-production-9d9169b0158d.herokuapp.com',
 };
